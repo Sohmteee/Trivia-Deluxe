7732723efadb7e9f56e8d88f8af5c4b0e4e221c1
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-23 19:34:51 +0100

    commit

--- lib/models/stat_bar.dart
+++ lib/models/stat_bar.dart
@@ -7,7 +7,10 @@ import 'package:trivia/providers/money.dart';
 class GameStats extends StatefulWidget {
   const GameStats({
     super.key,
+    this.showHome,
   });
+
+  final bool? showHome;
 
   @override
   State<GameStats> createState() => _GameStatsState();
@@ -21,199 +24,197 @@ class _GameStatsState extends State<GameStats> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<MoneyProvider>(
-      builder: (context, moneyProvider, _) {
-        return Row(
-          children: [
-            GestureDetector(
-              onTapDown: (details) {
-                Navigator.pushNamedAndRemoveUntil(
-                    context, "/menu", (route) => false);
-              },
-              child: Container(
-                padding: EdgeInsets.all(2.sp),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: AppColor.lightRed,
-                    width: 2.sp,
+    return Consumer<MoneyProvider>(builder: (context, moneyProvider, _) {
+      return Row(
+        children: [
+          if()GestureDetector(
+            onTapDown: (details) {
+              Navigator.pushNamedAndRemoveUntil(
+                  context, "/menu", (route) => false);
+            },
+            child: Container(
+              padding: EdgeInsets.all(2.sp),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: AppColor.lightRed,
+                  width: 2.sp,
+                ),
+                gradient: LinearGradient(
+                  colors: [
+                    AppColor.lightRed,
+                    AppColor.darkRed,
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+              child: Icon(
+                Icons.home_rounded,
+                size: 18.sp,
+                color: AppColor.levelYellow,
+              ),
+            ),
+          ),
+          const Spacer(flex: 4),
+          Container(
+            height: 20.h,
+            padding: EdgeInsets.all(2.sp),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: AppColor.lightRed,
+                width: 2.sp,
+              ),
+              borderRadius: BorderRadius.circular(8.r),
+              gradient: LinearGradient(
+                colors: [
+                  AppColor.lightRed,
+                  AppColor.darkRed,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Image.asset("assets/images/money.png"),
+          ),
+          const Spacer(),
+          Container(
+            height: 20.h,
+            width: 60.w,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: AppColor.lightRed,
+                width: 2.sp,
+              ),
+              borderRadius: BorderRadius.circular(15.r),
+              gradient: LinearGradient(
+                colors: [
+                  AppColor.lightRed,
+                  AppColor.darkRed,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      moneyProvider.cash.toString(),
+                      style: const TextStyle(
+                        color: Colors.yellow,
+                      ),
+                    ),
                   ),
-                  gradient: LinearGradient(
-                    colors: [
-                      AppColor.lightRed,
-                      AppColor.darkRed,
+                ),
+                Container(
+                  width: 18.w,
+                  padding: EdgeInsets.symmetric(vertical: 3.sp),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.horizontal(
+                      right: Radius.circular(8.r),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.add,
+                        size: 13.sp,
+                        color: AppColor.white,
+                      ),
+                      const SizedBox(),
                     ],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
                   ),
-                ),
-                child: Icon(
-                  Icons.home_rounded,
-                  size: 18.sp,
-                  color: AppColor.levelYellow,
-                ),
-              ),
+                )
+              ],
             ),
-            const Spacer(flex: 4),
-            Container(
-              height: 20.h,
-              padding: EdgeInsets.all(2.sp),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: AppColor.lightRed,
-                  width: 2.sp,
-                ),
-                borderRadius: BorderRadius.circular(8.r),
-                gradient: LinearGradient(
-                  colors: [
-                    AppColor.lightRed,
-                    AppColor.darkRed,
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
+          ),
+          const Spacer(flex: 3),
+          Container(
+            height: 20.h,
+            padding: EdgeInsets.all(2.sp),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: AppColor.lightRed,
+                width: 2.sp,
               ),
-              child: Image.asset("assets/images/money.png"),
-            ),
-            const Spacer(),
-            Container(
-              height: 20.h,
-              width: 60.w,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: AppColor.lightRed,
-                  width: 2.sp,
-                ),
-                borderRadius: BorderRadius.circular(15.r),
-                gradient: LinearGradient(
-                  colors: [
-                    AppColor.lightRed,
-                    AppColor.darkRed,
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Center(
-                      child: Text(
-                        moneyProvider.cash.toString(),
-                        style: const TextStyle(
-                          color: Colors.yellow,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: 18.w,
-                    padding: EdgeInsets.symmetric(vertical: 3.sp),
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.horizontal(
-                        right: Radius.circular(8.r),
-                      ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Icon(
-                          Icons.add,
-                          size: 13.sp,
-                          color: AppColor.white,
-                        ),
-                        const SizedBox(),
-                      ],
-                    ),
-                  )
+              borderRadius: BorderRadius.circular(8.r),
+              gradient: LinearGradient(
+                colors: [
+                  AppColor.lightRed,
+                  AppColor.darkRed,
                 ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
               ),
             ),
-            const Spacer(flex: 3),
-            Container(
-              height: 20.h,
-              padding: EdgeInsets.all(2.sp),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: AppColor.lightRed,
-                  width: 2.sp,
-                ),
-                borderRadius: BorderRadius.circular(8.r),
-                gradient: LinearGradient(
-                  colors: [
-                    AppColor.lightRed,
-                    AppColor.darkRed,
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
+            child: Image.asset("assets/images/coin.png"),
+          ),
+          const Spacer(),
+          Container(
+            height: 20.h,
+            width: 60.w,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
+                color: AppColor.lightRed,
+                width: 2.sp,
               ),
-              child: Image.asset("assets/images/coin.png"),
+              borderRadius: BorderRadius.circular(15.r),
+              gradient: LinearGradient(
+                colors: [
+                  AppColor.lightRed,
+                  AppColor.darkRed,
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
             ),
-            const Spacer(),
-            Container(
-              height: 20.h,
-              width: 60.w,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: AppColor.lightRed,
-                  width: 2.sp,
-                ),
-                borderRadius: BorderRadius.circular(15.r),
-                gradient: LinearGradient(
-                  colors: [
-                    AppColor.lightRed,
-                    AppColor.darkRed,
-                  ],
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                ),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Center(
-                      child: Text(
-                         moneyProvider.coins.toString(),
-                        style: const TextStyle(
-                          color: Colors.yellow,
-                        ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: Text(
+                      moneyProvider.coins.toString(),
+                      style: const TextStyle(
+                        color: Colors.yellow,
                       ),
                     ),
                   ),
-                  Container(
-                    width: 18.w,
-                    padding: EdgeInsets.symmetric(vertical: 3.sp),
-                    decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.horizontal(
-                        right: Radius.circular(8.r),
+                ),
+                Container(
+                  width: 18.w,
+                  padding: EdgeInsets.symmetric(vertical: 3.sp),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.horizontal(
+                      right: Radius.circular(8.r),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Icon(
+                        Icons.add,
+                        size: 13.sp,
+                        color: AppColor.white,
                       ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Icon(
-                          Icons.add,
-                          size: 13.sp,
-                          color: AppColor.white,
-                        ),
-                        const SizedBox(),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                      const SizedBox(),
+                    ],
+                  ),
+                )
+              ],
             ),
-          ],
-        );
-      }
-    );
+          ),
+        ],
+      );
+    });
   }
 }
