c49c36509c18c5ad0e383e664e2156c15bc9d307
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-23 15:05:06 +0100

    commit

--- lib/providers/money.dart
+++ lib/providers/money.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 class MoneyProvider extends ChangeNotifier{
-  int 
+  int _cash = 1280;
+  int _coins = 60;
+
+  int get cash => _cash;
+  int get coins => _coins;
+
+  increaseCash(int value) {
+    if (cash + value)
+  }
+
 }
