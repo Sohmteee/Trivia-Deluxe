dd740464ca6f9da79e919c66d555817ae2da4b17
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-23 15:39:34 +0100

    commit

--- lib/models/dialog.dart
+++ lib/models/dialog.dart
@@ -112,6 +112,27 @@ showFailedDialog(context, questionIndex) {
                             .shimmer(
                               delay: 2.seconds,
                               duration: .5.seconds,
+                            )
+                            .scaleXY(
+                              curve: Curves.easeOutSine,
+                              delay: 2.seconds,
+                              duration: .2.seconds,
+                              begin: 1,
+                              end: .8,
+                            )
+                            .then()
+                            .scaleXY(
+                              curve: Curves.easeOutSine,
+                              duration: .4.seconds,
+                              begin: .8,
+                              end: 1.2,
+                            )
+                            .then()
+                            .scaleXY(
+                              curve: Curves.bounceOut,
+                              duration: .4.seconds,
+                              begin: 1.2,
+                              end: 1,
                             ),
                       ),
                     ),
