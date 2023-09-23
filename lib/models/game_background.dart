e7a97ed091e4a7680a2dc6dffc8b4551529c560c
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-23 20:10:37 +0100

    commit

--- lib/models/game_background.dart
+++ lib/models/game_background.dart
@@ -2,7 +2,6 @@ import 'package:blur/blur.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trivia/colors/app_color.dart';
-import 'package:trivia/models/pop_scope.dart';
 
 class GameBackground extends StatefulWidget {
   const GameBackground({
