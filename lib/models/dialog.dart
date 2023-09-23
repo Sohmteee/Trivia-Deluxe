3d7960c09aa0f0e71c274060c8c9a2ab2c53e3b6
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-23 14:07:46 +0100

    commit

--- lib/models/dialog.dart
+++ lib/models/dialog.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:trivia/colors/app_color.dart';
 
 showFailedDialog(context) {
   showDialog(
       context: context,
       builder: (context) {
         return Theme(
-          
           data: Theme.of(context).copyWith(
             dialogBackgroundColor: Colors.transparent,
+          ),
           child: Dialog(
+            elevation: 0,
+            shadowColor: Colors.transparent,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            insetAnimationCurve: Curves.bounceInOut,
+            insetAnimationDuration: const Duration(milliseconds: 300),
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 40),
+              decoration: BoxDecoration(
+                color: AppColor.darkRed,
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
           ),
         );
       });
