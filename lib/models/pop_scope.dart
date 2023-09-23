927dfd9ebebd647bf677e55010426c21c3dce298
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-23 20:00:34 +0100

    commit

--- lib/models/pop_scope.dart
+++ lib/models/pop_scope.dart
@@ -1,12 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:toast/toast.dart';
 
 Future<bool> onWillPop(context) async {
   ToastContext().init(context);
-  Toast.show(
-    "You can't go back at the moment",
-    duration: 2,
-    gravity: Toast.bottom,
-    
-  );
+  Toast.show("You can't go back at the moment",
+      duration: 2,
+      gravity: Toast.top,
+      textStyle: TextStyle(
+        fontSize: 20.sp,
+      ));
   return false;
 }
