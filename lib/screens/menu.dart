a840d922d5876c8023800a254b48e45ea8176127
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-24 22:34:21 +0100

    commit

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -16,94 +16,96 @@ class MenuScreen extends StatefulWidget {
 class _MenuScreenState extends State<MenuScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/house.jpg"),
-            fit: BoxFit.fill,
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/house.jpg"),
+              fit: BoxFit.fill,
+            ),
           ),
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            color: AppColor.black.withOpacity(.3),
-          ),
-          child: Center(
-            child: Column(
-              children: [
-                const Spacer(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 25.w),
-                  child: const GameStats(showHome: false),
-                ),
-                const Spacer(flex: 3),
-                Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Text(
-                      "TRIVIA\nDELUXE",
-                      style: TextStyle(
-                        color: const Color.fromARGB(255, 150, 22, 13),
-                        fontSize: 90.sp,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    Text(
-                      "TRIVIA\nDELUXE",
-                      style: TextStyle(
-                        color: AppColor.white,
-                        fontSize: 80.sp,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                )
-                    .animate(
-                      onPlay: (controller) => controller.repeat(),
-                    )
-                    .shimmer(
-                      delay: 2.seconds,
-                      duration: 1.seconds,
-                    ),
-                const Spacer(flex: 2),
-                SizedBox(
-                  height: 70.w,
-                  child: ZoomTapAnimation(
-                    onTap: () {
-                      Navigator.pushNamed(context, "/level");
-                    },
-                    child: Image.asset("assets/images/play.png"),
+          child: Container(
+            decoration: BoxDecoration(
+              color: AppColor.black.withOpacity(.3),
+            ),
+            child: Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 10.h),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25.w),
+                    child: const GameStats(showHome: false),
                   ),
-                )
-                    .animate(
-                      onPlay: (controller) => controller.repeat(),
-                    )
-                    .then()
-                    .scaleXY(
-                      curve: Curves.easeOutSine,
-                      delay: 2.seconds,
-                      duration: .2.seconds,
-                      begin: 1,
-                      end: .8,
-                    )
-                    .then()
-                    .scaleXY(
-                      curve: Curves.easeOutSine,
-                      duration: .4.seconds,
-                      begin: .8,
-                      end: 1.2,
-                    )
-                    .then()
-                    .scaleXY(
-                      curve: Curves.bounceOut,
-                      duration: .4.seconds,
-                      begin: 1.2,
-                      end: 1,
+                  const Spacer(flex: 3),
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Text(
+                        "TRIVIA\nDELUXE",
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 150, 22, 13),
+                          fontSize: 90.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        "TRIVIA\nDELUXE",
+                        style: TextStyle(
+                          color: AppColor.white,
+                          fontSize: 80.sp,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  )
+                      .animate(
+                        onPlay: (controller) => controller.repeat(),
+                      )
+                      .shimmer(
+                        delay: 2.seconds,
+                        duration: 1.seconds,
+                      ),
+                  const Spacer(flex: 2),
+                  SizedBox(
+                    height: 70.w,
+                    child: ZoomTapAnimation(
+                      onTap: () {
+                        Navigator.pushNamed(context, "/level");
+                      },
+                      child: Image.asset("assets/images/play.png"),
                     ),
-                const Spacer(),
-              ],
+                  )
+                      .animate(
+                        onPlay: (controller) => controller.repeat(),
+                      )
+                      .then()
+                      .scaleXY(
+                        curve: Curves.easeOutSine,
+                        delay: 2.seconds,
+                        duration: .2.seconds,
+                        begin: 1,
+                        end: .8,
+                      )
+                      .then()
+                      .scaleXY(
+                        curve: Curves.easeOutSine,
+                        duration: .4.seconds,
+                        begin: .8,
+                        end: 1.2,
+                      )
+                      .then()
+                      .scaleXY(
+                        curve: Curves.bounceOut,
+                        duration: .4.seconds,
+                        begin: 1.2,
+                        end: 1,
+                      ),
+                  const Spacer(),
+                ],
+              ),
             ),
           ),
         ),
