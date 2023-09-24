241dc8ac6087d5264907375ac7a987655d20ca8e
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-24 22:47:24 +0100

    commit

--- lib/models/dialogs/dialog_background.dart
+++ lib/models/dialogs/dialog_background.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
 import 'package:trivia/colors/app_color.dart';
 import 'package:trivia/models/pop_scope.dart';
 import 'package:trivia/providers/money.dart';
-import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 showGameDialog(context, {required Widget child}) {
   showDialog(
@@ -29,94 +27,7 @@ showGameDialog(context, {required Widget child}) {
                     color: AppColor.lightRed,
                     borderRadius: BorderRadius.circular(20),
                   ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        "Oya another option dey",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25.sp,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: 10.h),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15.w),
-                        child: Text(
-                          "Would you like to watch an ad instead?",
-                          style: TextStyle(
-                            color: Colors.grey[400],
-                            fontSize: 20.sp,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      SizedBox(height: 20.h),
-                      ZoomTapAnimation(
-                        onTap: () {
-                          Navigator.pushReplacementNamed(context, "/level");
-                        },
-                        child: Container(
-                          width: double.maxFinite,
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20.sp, vertical: 10.sp),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20.r),
-                          ),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Okay I fit do that one",
-                                style: TextStyle(
-                                  fontSize: 15.sp,
-                                ),
-                              ),
-                              SizedBox(width: 10.w),
-                              SizedBox(
-                                height: 20.h,
-                                child: Image.asset("assets/images/youtube.png"),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 10.h),
-                      ZoomTapAnimation(
-                        onTap: () {},
-                        child: Container(
-                          width: double.maxFinite,
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20.sp, vertical: 10.sp),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20.r),
-                          ),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                "Omo I dey manage my data too",
-                                style: TextStyle(
-                                  fontSize: 15.sp,
-                                ),
-                              ),
-                              SizedBox(width: 10.w),
-                              SizedBox(
-                                height: 20.h,
-                                child: Image.asset("assets/images/laugh.png"),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  child: child,
                 ),
               ),
             );
