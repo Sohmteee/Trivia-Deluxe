911d6bf3d58bd5f9dabd89a059454838b262b511
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-24 22:52:55 +0100

    commit

--- lib/models/dialogs/fail_dialog.dart
+++ lib/models/dialogs/fail_dialog.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-import 'package:trivia/colors/app_color.dart';
 import 'package:trivia/data/questions.dart';
 import 'package:trivia/models/dialogs/low_cash_dialog.dart';
-import 'package:trivia/models/pop_scope.dart';
 import 'package:trivia/providers/money.dart';
 import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
@@ -57,85 +55,89 @@ showFailedDialog(context, questionIndex, bool timeUp) {
                           ],
                         ),
                       SizedBox(height: 20.h),
-                      ZoomTapAnimation(
-                        onTap: () {
-                          if (moneyProvider.coins - 5 >= 0) {
-                            moneyProvider.decreaseCoins(5);
-                            Navigator.pushReplacementNamed(context, "/game");
-                          } else {
-                            Navigator.pop(context);
-                            showLowCashDialog(context);
-                          }
-                        },
-                        child: Container(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20.sp, vertical: 10.sp),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20.r),
-                          ),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                "Revive",
-                                style: TextStyle(
-                                  fontSize: 25.sp,
-                                ),
+                      Consumer<MoneyProvider>(
+                        builder: (context, moneyProvider, _) {
+                          return ZoomTapAnimation(
+                            onTap: () {
+                              if (moneyProvider.coins - 5 >= 0) {
+                                moneyProvider.decreaseCoins(5);
+                                Navigator.pushReplacementNamed(context, "/game");
+                              } else {
+                                Navigator.pop(context);
+                                showLowCashDialog(context);
+                              }
+                            },
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 20.sp, vertical: 10.sp),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(20.r),
                               ),
-                              SizedBox(width: 10.w),
-                              Stack(
-                                clipBehavior: Clip.none,
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
                                 children: [
-                                  SizedBox(
-                                    height: 20.h,
-                                    child:
-                                        Image.asset("assets/images/coin.png"),
-                                  ),
-                                  Positioned(
-                                    top: -5,
-                                    right: -6,
-                                    child: Text(
-                                      "\u00d75",
-                                      style: TextStyle(
-                                        fontSize: 20.sp,
-                                      ),
+                                  Text(
+                                    "Revive",
+                                    style: TextStyle(
+                                      fontSize: 25.sp,
                                     ),
+                                  ),
+                                  SizedBox(width: 10.w),
+                                  Stack(
+                                    clipBehavior: Clip.none,
+                                    children: [
+                                      SizedBox(
+                                        height: 20.h,
+                                        child:
+                                            Image.asset("assets/images/coin.png"),
+                                      ),
+                                      Positioned(
+                                        top: -5,
+                                        right: -6,
+                                        child: Text(
+                                          "\u00d75",
+                                          style: TextStyle(
+                                            fontSize: 20.sp,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ],
                               ),
-                            ],
-                          ),
-                        ),
-                      )
-                          .animate(
-                            onPlay: (controller) => controller.repeat(),
+                            ),
                           )
-                          .shimmer(
-                            delay: 2.seconds,
-                            duration: .7.seconds,
-                          )
-                          .scaleXY(
-                            curve: Curves.easeOutSine,
-                            delay: 2.seconds,
-                            duration: .1.seconds,
-                            begin: 1,
-                            end: .8,
-                          )
-                          .then()
-                          .scaleXY(
-                            curve: Curves.easeOutSine,
-                            duration: .4.seconds,
-                            begin: .8,
-                            end: 1.2,
-                          )
-                          .then()
-                          .scaleXY(
-                            curve: Curves.bounceOut,
-                            duration: .2.seconds,
-                            begin: 1.2,
-                            end: 1,
-                          ),
+                              .animate(
+                                onPlay: (controller) => controller.repeat(),
+                              )
+                              .shimmer(
+                                delay: 2.seconds,
+                                duration: .7.seconds,
+                              )
+                              .scaleXY(
+                                curve: Curves.easeOutSine,
+                                delay: 2.seconds,
+                                duration: .1.seconds,
+                                begin: 1,
+                                end: .8,
+                              )
+                              .then()
+                              .scaleXY(
+                                curve: Curves.easeOutSine,
+                                duration: .4.seconds,
+                                begin: .8,
+                                end: 1.2,
+                              )
+                              .then()
+                              .scaleXY(
+                                curve: Curves.bounceOut,
+                                duration: .2.seconds,
+                                begin: 1.2,
+                                end: 1,
+                              );
+                        }
+                      ),
                     ],
                   ),
       );
