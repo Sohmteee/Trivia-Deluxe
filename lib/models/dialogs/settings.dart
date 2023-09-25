8b05e7c5bd6b66e431e6b7f91d5207db448c9510
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-25 23:39:04 +0100

    commit

--- lib/models/dialogs/settings.dart
+++ lib/models/dialogs/settings.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-import 'package:trivia/colors/app_color.dart';
 import 'package:trivia/models/dialogs/game_dialog.dart';
 import 'package:trivia/providers/audio.dart';
 
@@ -49,6 +48,7 @@ showSettingsDialog(BuildContext context) {
                     return Slider(
                       divisions: 10,
                       value: audioProvider.volume,
+                      thumbColor: Colors.d,
                       onChanged: (value) {
                         audioProvider.setVolume(value);
                       },
