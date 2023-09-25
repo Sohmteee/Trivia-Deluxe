25e31e286d725c234edd5e177f1721ec2404379d
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-25 22:52:39 +0100

    commit

--- lib/providers/audio.dart
+++ lib/providers/audio.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:trivia/data/controllers.dart';
 
 class AudioProvider extends ChangeNotifier {
   double volume = 1;
 
   setVolume(double newVolume) {
     volume = newVolume;
+    player.volume(newVolume);
     notifyListeners();
   }
 }
