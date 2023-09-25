960f2efa7dfa9ef064d197290a7dd039f7c54f51
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-25 23:48:33 +0100

    commit

--- lib/providers/audio.dart
+++ lib/providers/audio.dart
@@ -3,6 +3,8 @@ import 'package:trivia/data/controllers.dart';
 
 class AudioProvider extends ChangeNotifier {
   double volume = 1;
+  bool musicOn = true;
+  bool musicOn = true;
 
   setVolume(double newVolume) {
     volume = newVolume;
