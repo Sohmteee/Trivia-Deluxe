6c5cab919d71894b0ac61cc82b9be3936005c0b2
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-25 22:36:18 +0100

    commit

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -13,19 +13,39 @@ class MenuScreen extends StatefulWidget {
   State<MenuScreen> createState() => _MenuScreenState();
 }
 
-class _MenuScreenState extends State<MenuScreen> with {
+class _MenuScreenState extends State<MenuScreen> with WidgetsBindingObserver {
   
  @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addObserver(this);
-    ...
+    playBGAudio();
   }
 
   @override
   void dispose() {
-    WidgetsBinding.instance?.removeObserver(this);
+    WidgetsBinding.instance.removeObserver(this);
     super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.resumed:
+        print("app in resumed");
+        break;
+      case AppLifecycleState.inactive:
+        print("app in inactive");
+        break;
+      case AppLifecycleState.paused:
+        print("app in paused");
+        break;
+      case AppLifecycleState.detached:
+        print("app in detached");
+        break;
+      case AppLifecycleState.hidden:
+        print("app in hidden");
+    }
   }
 
   Future<void> playBGAudio() async {
