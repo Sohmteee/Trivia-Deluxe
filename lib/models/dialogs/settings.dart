4281a6b90b34b3a868f2c1ee226f3e94f1a50227
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-25 22:58:06 +0100

    commit

--- lib/models/dialogs/settings.dart
+++ lib/models/dialogs/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trivia/models/dialogs/game_dialog.dart';
 
 showSettingsDialog(BuildContext context) {
@@ -8,13 +9,25 @@ showSettingsDialog(BuildContext context) {
       mainAxisSize: MainAxisSize.min,
       children: [
         Text(
-          "Oya another option dey",
+          "Game Settings",
           style: TextStyle(
             color: Colors.white,
             fontSize: 25.sp,
             fontWeight: FontWeight.bold,
           ),
           textAlign: TextAlign.center,
+        ),
+        SizedBox(height: 10.h),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.w),
+          child: Text(
+            "Would you like to watch an ad instead?",
+            style: TextStyle(
+              color: Colors.grey[400],
+              fontSize: 20.sp,
+            ),
+            textAlign: TextAlign.center,
+          ),
         ),
       ],
     ),
