a95bff63014445a117ad8713b8ff9d77b7c445e3
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-25 01:01:58 +0100

    commit

---
+++ lib/screens/select.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:trivia/models/game_background.dart';
+
+class SelectSreen extends StatefulWidget {
+  const SelectSreen({super.key});
+
+  @override
+  State<SelectSreen> createState() => _SelectSreenState();
+}
+
+class _SelectSreenState extends State<SelectSreen> {
+  @override
+  Widget build(BuildContext context) {
+    return GameBackground(body: );
+  }
+}
