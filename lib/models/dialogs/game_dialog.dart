8ec26303d9b74794d86aeb23e9bd282fa42bbd9d
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-26 00:24:52 +0100

    commit

--- lib/models/dialogs/game_dialog.dart
+++ lib/models/dialogs/game_dialog.dart
@@ -14,23 +14,44 @@ showGameDialog(BuildContext context,
           ),
           child: WillPopScope(
             onWillPop: () => onWillPop(context, isExitable ?? false),
-            child: Dialog(
-              elevation: 0,
-              shadowColor: Colors.transparent,
-              clipBehavior: Clip.antiAliasWithSaveLayer,
-              insetAnimationCurve: Curves.bounceInOut,
-              insetAnimationDuration: const Duration(milliseconds: 300),
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(20, 40, 20, 60),
-                decoration: BoxDecoration(
-                  color: AppColor.lightRed,
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(
-                    color: Colors.white,
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Dialog(
+                  elevation: 0,
+                  shadowColor: Colors.transparent,
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  insetAnimationCurve: Curves.bounceInOut,
+                  insetAnimationDuration: const Duration(milliseconds: 300),
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(20, 40, 20, 60),
+                    decoration: BoxDecoration(
+                      color: AppColor.lightRed,
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: child,
                   ),
                 ),
-                child: child,
-              ),
+                if (isExitable ?? false)
+                  Positioned(
+                    top: 250,
+                    right: 30,
+                    child: Container(
+                      padding: EdgeInsets.all(5.r),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                        ),
+                      ),
+                      child: Icon(Icons.close),
+                    ),
+                  )
+              ],
             ),
           ),
         );
