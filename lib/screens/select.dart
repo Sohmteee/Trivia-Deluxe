4c9a39a5759434867986ded40fcfc4511cb09fb4
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-25 01:03:05 +0100

    commit

--- lib/screens/select.dart
+++ lib/screens/select.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trivia/models/game_background.dart';
+import 'package:trivia/models/stat_bar.dart';
 
 class SelectSreen extends StatefulWidget {
   const SelectSreen({super.key});
@@ -11,6 +13,17 @@ class SelectSreen extends StatefulWidget {
 class _SelectSreenState extends State<SelectSreen> {
   @override
   Widget build(BuildContext context) {
-    return GameBackground(body: );
+    return GameBackground(
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 40.w, vertical: 40.h),
+        child: Column(
+          children: [
+            const GameStats(),
+            const Spacer(),
+            const Spacer(flex: 3),
+          ],
+        ),
+      ),
+    );
   }
 }
