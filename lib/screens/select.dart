bc3d46da1d12113ec2babb28bc3f36aa1a2a8737
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-25 01:05:17 +0100

    commit

--- lib/screens/select.dart
+++ lib/screens/select.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:trivia/colors/app_color.dart';
 import 'package:trivia/models/game_background.dart';
 import 'package:trivia/models/level_tile.dart';
 import 'package:trivia/models/stat_bar.dart';
@@ -26,9 +27,68 @@ class _SelectSreenState extends State<SelectSreen> {
               padding: EdgeInsets.only(top: 20.h),
               physics: const NeverScrollableScrollPhysics(),
               itemBuilder: (BuildContext context, int index) {
-                return LevelTile(
-                  level: index + 1,
-                );
+                return Consumer<LevelProvider>(
+                    builder: (_, levelProvider, child) {
+                  return widget.level == levelProvider.completedLevel + 1
+                      ? SizedBox(
+                          child: Container(
+                            padding: EdgeInsets.all(20.sp),
+                            margin: EdgeInsets.only(bottom: 20.sp),
+                            decoration: BoxDecoration(
+                              color:
+                                  levelProvider.completedLevel >= widget.level
+                                      ? AppColor.levelYellow
+                                      : widget.level ==
+                                              levelProvider.completedLevel + 1
+                                          ? AppColor.levelYellow.withOpacity(.3)
+                                          : Colors.transparent,
+                              border: Border.all(
+                                width: 2.sp,
+                                color: AppColor.lightRed,
+                              ),
+                            ),
+                            child: Text(
+                              (widget.level).toString(),
+                              style: TextStyle(
+                                color: AppColor.white,
+                                fontSize: 16.sp,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          )
+                          )
+                      : Container(
+                          padding: EdgeInsets.all(20.sp),
+                          margin: EdgeInsets.only(bottom: 20.sp),
+                          decoration: BoxDecoration(
+                            color: levelProvider.completedLevel >= widget.level
+                                ? AppColor.levelYellow
+                                : widget.level ==
+                                        levelProvider.completedLevel + 1
+                                    ? AppColor.levelYellow.withOpacity(.3)
+                                    : Colors.transparent,
+                            border: Border.all(
+                              width: 2.sp,
+                              color: AppColor.lightRed,
+                            ),
+                          ),
+                          child: Text(
+                            (widget.level).toString(),
+                            style: TextStyle(
+                              color: AppColor.white,
+                              fontSize: 16.sp,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                          .animate(
+                            onPlay: (controller) => controller.repeat(),
+                          )
+                          .shimmer(
+                            delay: 1.5.seconds,
+                            duration: 1.seconds,
+                          );
+                });
               },
             ),
             const Spacer(flex: 3),
