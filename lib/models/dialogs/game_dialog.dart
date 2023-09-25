a3ac3ccea12ba324be51cf93adc774c73e1b1c24
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-26 00:19:19 +0100

    commit

--- lib/models/dialogs/game_dialog.dart
+++ lib/models/dialogs/game_dialog.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trivia/colors/app_color.dart';
 import 'package:trivia/models/pop_scope.dart';
 
-showGameDialog(BuildContext context, {required Widget child, bool? isExitable}) {
+showGameDialog(BuildContext context,
+    {required Widget child, bool? isExitable}) {
   showDialog(
       context: context,
       builder: (context) {
@@ -12,23 +14,44 @@ showGameDialog(BuildContext context, {required Widget child, bool? isExitable}) 
           ),
           child: WillPopScope(
             onWillPop: () => onWillPop(context, isExitable ?? false),
-            child: Dialog(
-              elevation: 0,
-              shadowColor: Colors.transparent,
-              clipBehavior: Clip.antiAliasWithSaveLayer,
-              insetAnimationCurve: Curves.bounceInOut,
-              insetAnimationDuration: const Duration(milliseconds: 300),
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(20, 40, 20, 60),
-                decoration: BoxDecoration(
-                  color: AppColor.lightRed,
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(
-                    color: Colors.white,
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Dialog(
+                  elevation: 0,
+                  shadowColor: Colors.transparent,
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  insetAnimationCurve: Curves.bounceInOut,
+                  insetAnimationDuration: const Duration(milliseconds: 300),
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(20, 40, 20, 60),
+                    decoration: BoxDecoration(
+                      color: AppColor.lightRed,
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(
+                        color: Colors.white,
+                      ),
+                    ),
+                    child: child,
                   ),
                 ),
-                child: child,
-              ),
+                if (isExitable ?? false)
+                  Positioned(
+                    top: -10,
+                    right: -10,
+                    child: Container(
+                      padding: EdgeInsets.all(5.r),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                        ),
+                      ),
+                      child: Icon(Icons.close),
+                    ),
+                  )
+              ],
             ),
           ),
         );
