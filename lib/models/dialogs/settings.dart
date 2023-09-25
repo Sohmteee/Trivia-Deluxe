9dd2a2b8fa0bcf09a00b804d084a6baaf702e3d4
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-25 22:56:59 +0100

    commit

--- lib/models/dialogs/settings.dart
+++ lib/models/dialogs/settings.dart
@@ -1,1 +1,22 @@
-showSettingsDialog(context)
+import 'package:flutter/material.dart';
+import 'package:trivia/models/dialogs/game_dialog.dart';
+
+showSettingsDialog(BuildContext context) {
+  showGameDialog(
+    context,
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          "Oya another option dey",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.sp,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    ),
+  );
+}
