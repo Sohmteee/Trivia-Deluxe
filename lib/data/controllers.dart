fdce0e759e70fea30d9cb6b710210c90d7c37424
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-26 14:14:33 +0100

    commit

--- lib/data/controllers.dart
+++ lib/data/controllers.dart
@@ -2,4 +2,5 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 
 final countDownController = CountDownController();
+final adCountDownController = CountDownController();
 final player = AudioPlayer();
--- lib/screens/ad.dart
+++ lib/screens/ad.dart
@@ -1,7 +1,9 @@
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trivia/colors/app_color.dart';
+import 'package:trivia/data/controllers.dart';
 import 'package:trivia/models/dialogs/close_ad.dart';
 import 'package:trivia/models/dialogs/exit.dart';
 import 'package:trivia/models/dialogs/settings.dart';
@@ -30,15 +32,15 @@ class _AdScreenState extends State<AdScreen> with WidgetsBindingObserver {
                 alignment: Alignment.topRight,
                 child: CircularCountDownTimer(
                   duration: 30,
-                  controller: countDownController,
+                  controller: adCountDownController,
                   width: 70,
                   height: 70,
                   ringColor: Colors.transparent,
                   ringGradient: null,
                   fillColor: Colors.grey[300]!,
                   fillGradient: null,
-                  backgroundColor: getColor(iterationCount - 1),
-                  backgroundGradient: null,
+                  backgroundColor: Colors.transparent,
+                  backgroundGradient: Colors.grey[500]!,
                   strokeWidth: 3,
                   strokeCap: StrokeCap.round,
                   textStyle: TextStyle(
