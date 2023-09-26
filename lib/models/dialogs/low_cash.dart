121abb7062ee0859cee4dc95b0518f9abbdea715
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-26 15:13:44 +0100

    commit

--- lib/models/dialogs/low_cash.dart
+++ lib/models/dialogs/low_cash.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:provider/provider.dart';
-import 'package:trivia/providers/money.dart';
 import 'package:zoom_tap_animation/zoom_tap_animation.dart';
 
 import 'game.dart';
