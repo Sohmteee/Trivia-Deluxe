adaa13b4cf941de09500df981d94b96b236c6899
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-26 14:18:13 +0100

    commit

--- lib/screens/ad.dart
+++ lib/screens/ad.dart
@@ -25,51 +25,56 @@ class _AdScreenState extends State<AdScreen> with WidgetsBindingObserver {
             children: [
               Align(
                 alignment: Alignment.topRight,
-                child: CircularCountDownTimer(
-                  duration: 5,
-                  controller: adCountDownController,
-                  width: 20,
-                  height: 20,
-                  ringColor: Colors.grey,
-                  ringGradient: null,
-                  fillColor: Colors.grey[300]!,
-                  fillGradient: null,
-                  backgroundColor: Colors.transparent,
-                  backgroundGradient: null,
-                  strokeWidth: 3,
-                  strokeCap: StrokeCap.round,
-                  textStyle: TextStyle(
-                    fontSize: 33.sp,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: CircularCountDownTimer(
+                    duration: 5,
+                    controller: adCountDownController,
+                    width: 20,
+                    height: 20,
+                    ringColor: Colors.grey,
+                    ringGradient: null,
+                    fillColor: Colors.grey[300]!,
+                    fillGradient: null,
+                    backgroundColor: Colors.transparent,
+                    backgroundGradient: null,
+                    strokeWidth: 3,
+                    strokeCap: StrokeCap.round,
+                    textStyle: TextStyle(
+                      fontSize: 33.sp,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textFormat: CountdownTextFormat.S,
+                    isReverse: true,
+                    isReverseAnimation: true,
+                    isTimerTextShown: true,
+                    autoStart: true,
+                    onStart: () {
+                      debugPrint('Countdown Started');
+
+                      /* timer = Timer.periodic(1.seconds, (timer) {
+                        if (iterationCount > 0) {
+                          setState(() {
+                            iterationCount--;
+                          });
+                        } else {
+                          timer.cancel();
+                        }
+                      });
+                     */
+                    },
+                    onComplete: () {
+                      debugPrint('Countdown Ended');
+
+                      // questionProvider.checkCorrectAnswer(context, -1);
+                    },
+                    timeFormatterFunction:
+                        (defaultFormatterFunction, duration) {
+                      return Function.apply(
+                          defaultFormatterFunction, [duration]);
+                    },
                   ),
-                  textFormat: CountdownTextFormat.S,
-                  isReverse: true,
-                  isReverseAnimation: true,
-                  isTimerTextShown: true,
-                  autoStart: true,
-                  onStart: () {
-                    debugPrint('Countdown Started');
-
-                    /* timer = Timer.periodic(1.seconds, (timer) {
-                      if (iterationCount > 0) {
-                        setState(() {
-                          iterationCount--;
-                        });
-                      } else {
-                        timer.cancel();
-                      }
-                    });
-                   */
-                  },
-                  onComplete: () {
-                    debugPrint('Countdown Ended');
-
-                    // questionProvider.checkCorrectAnswer(context, -1);
-                  },
-                  timeFormatterFunction: (defaultFormatterFunction, duration) {
-                    return Function.apply(defaultFormatterFunction, [duration]);
-                  },
                 ),
               )
             ],
