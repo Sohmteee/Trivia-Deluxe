008dc446cf6b22248255da82e5eb0e746ce5c990
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-26 14:32:07 +0100

    commit

--- lib/screens/ad.dart
+++ lib/screens/ad.dart
@@ -72,7 +72,7 @@ class _AdScreenState extends State<AdScreen> with WidgetsBindingObserver {
                     timeFormatterFunction:
                         (defaultFormatterFunction, duration) {
                       if (duration.inSeconds == 5) {
-                        return Text("\u00d7");
+                        return "\u00d7";
                       } else {
                         return Function.apply(
                             defaultFormatterFunction, [duration]);
