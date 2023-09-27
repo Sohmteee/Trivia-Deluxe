6901f7a47cb400e6b444e1bac678ed029b593589
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-27 09:21:34 +0100

    commit

--- lib/models/dialogs/low_cash.dart
+++ lib/models/dialogs/low_cash.dart
@@ -37,7 +37,8 @@ showLowCashDialog(BuildContext context) {
           SizedBox(height: 20.h),
           ZoomTapAnimation(
             onTap: () {
-              Navigator.of(dialogContext).pop();
+              Navigator.pop(context);
+              Navigator.pop(context);
               Navigator.pushReplacementNamed(dialogContext, "/ad");
             },
             child: Container(
@@ -67,42 +68,41 @@ showLowCashDialog(BuildContext context) {
             ),
           ),
           SizedBox(height: 10.h),
-          Consumer<LevelProvider>(
-            builder: (context, levelProvider, _) {
-              return ZoomTapAnimation(
-                onTap: () {
-                  Navigator.of(dialogContext).pop();
-                  levelProvider.resetCompletedLevel();
-                  Navigator.popAndPushNamed(dialogContext, "/menu");
-                },
-                child: Container(
-                  width: double.maxFinite,
-                  padding: EdgeInsets.symmetric(horizontal: 20.sp, vertical: 10.sp),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20.r),
-                  ),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Omo I'm managing my data",
-                        style: TextStyle(
-                          fontSize: 15.sp,
-                        ),
-                      ),
-                      SizedBox(width: 10.w),
-                      SizedBox(
-                        height: 20.h,
-                        child: Image.asset("assets/images/laugh.png"),
-                      ),
-                    ],
-                  ),
+          Consumer<LevelProvider>(builder: (context, levelProvider, _) {
+            return ZoomTapAnimation(
+              onTap: () {
+                Navigator.of(dialogContext).pop();
+                levelProvider.resetCompletedLevel();
+                Navigator.popAndPushNamed(dialogContext, "/menu");
+              },
+              child: Container(
+                width: double.maxFinite,
+                padding:
+                    EdgeInsets.symmetric(horizontal: 20.sp, vertical: 10.sp),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.r),
                 ),
-              );
-            }
-          ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Omo I'm managing my data",
+                      style: TextStyle(
+                        fontSize: 15.sp,
+                      ),
+                    ),
+                    SizedBox(width: 10.w),
+                    SizedBox(
+                      height: 20.h,
+                      child: Image.asset("assets/images/laugh.png"),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          }),
         ],
       );
     }),
