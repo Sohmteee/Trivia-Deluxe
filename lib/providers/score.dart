681b25306f118ae19ba9f6e4fefa7f557b6b9f6b
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-28 10:54:07 +0100

    commit

--- lib/providers/score.dart
+++ lib/providers/score.dart
@@ -5,19 +5,18 @@ class ScoreProvider extends ChangeNotifier {
   int get score => _score;
 
   void updatedScore(int newScore) {
-    debugPrint("Score: $newScore");
     if (newScore <= 5) {
       _score += 1;
     } else if (newScore <= 10) {
-      _score += 1;
+      _score += 2;
     } else if (newScore <= 15) {
-      _score += 1;
+      _score += 3;
     } else if (newScore <= 20) {
-      _score += 1;
+      _score += 4;
     } else if (newScore <= 25) {
-      _score += 1;
-    } else if (newScore <= 5) {
-      _score += 1;
+      _score += 5;
+    } else if (newScore <= 30) {
+      _score += 6;
     }
     debugPrint("Curent Score: $score");
     notifyListeners();
