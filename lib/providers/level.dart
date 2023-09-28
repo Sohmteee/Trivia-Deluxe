934efe2251430f55d300d68cc634fecb61e31e5f
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-28 10:49:22 +0100

    commit

--- lib/providers/level.dart
+++ lib/providers/level.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:trivia/data/box.dart';
 
 class LevelProvider extends ChangeNotifier {
-  int _completedLevel = box.get("completedLevel") ?? 0;
+  int _completedLevel = 0;
   get completedLevel => _completedLevel;
 
   void incrementCompletedLevel() {
-    debugPrint("Completed Level: $completedLevel");
     if (completedLevel < 3) {
       _completedLevel++;
-      box.put("completedLevel", _completedLevel);
     }
-    debugPrint("New Completed Level: $completedLevel");
     notifyListeners();
   }
 
   void resetCompletedLevel() {
     _completedLevel = 0;
-    box.put("completedLevel", _completedLevel);
     notifyListeners();
   }
 }
