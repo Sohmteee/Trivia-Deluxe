b47eac76d1888983dc48dd7b26668f6b7ea204e9
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-29 15:00:36 +0100

    commit

--- lib/models/circle_border.dart
+++ lib/models/circle_border.dart
@@ -1,20 +1,22 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class CircleBorderPainter extends CustomPainter {
   Color color;
   double radius, angle;
 
-  CircleBorderPainter({required this.color, required this.radius, this.angle = 0});
+  CircleBorderPainter({
+    required this.color,
+    required this.radius,
+    this.angle = 0,
+  });
 
   @override
   void paint(Canvas canvas, Size size) {
     final circle = Paint()
       ..color = color
       ..style = PaintingStyle.stroke;
-    final arcRect = Rect.fromCircle(
-        center: size.bottomCenter(Offset.zero), radius: radius);
+    final arcRect =
+        Rect.fromCircle(center: size.bottomCenter(Offset.zero), radius: radius);
     canvas.drawArc(arcRect, 0, angle, false, circle);
   }
 
