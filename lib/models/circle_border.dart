9bbf8d384bfb634c7e46dcbfed8ce3571edbfb40
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-29 14:54:03 +0100

    commit

---
+++ lib/models/circle_border.dart
@@ -0,0 +1,18 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final redCircle = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke;
+    final arcRect = Rect.fromCircle(
+        center: size.bottomCenter(Offset.zero), radius: size.shortestSide);
+    canvas.drawArc(arcRect, 0, -pi, false, redCircle);
+  }
+
+  @override
+  bool shouldRepaint(MyPainter oldDelegate) => false;
+}
