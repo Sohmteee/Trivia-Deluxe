2755e79fdc36542f6f5e6827712cb693406e2a47
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-09-30 14:56:57 +0100

    commit

--- lib/models/pop_scope.dart
+++ lib/models/pop_scope.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:toast/toast.dart';
+import 'package:trivia/main.dart';
 
 Future<bool> onWillPop(context, isExitable) async {
   if (isExitable) {
     return true;
   }
+  playUnavailable(context);
   ToastContext().init(context);
   Toast.show("You can't go back at the moment",
       duration: 2,
