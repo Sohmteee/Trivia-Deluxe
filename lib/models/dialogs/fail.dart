5f60efdce0c337fad8670063251b1ca87044c5bf
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-04 10:40:19 +0100

    commit

--- lib/models/dialogs/fail.dart
+++ lib/models/dialogs/fail.dart
@@ -22,15 +22,13 @@ showFailedDialog(BuildContext context, questionIndex, bool timeUp) {
       mainAxisSize: MainAxisSize.min,
       children: [
         Container(
-          padding: EdgeInsets.all(35.sp),
-          decoration: BoxDecoration(
-            color: timeUp ? Colors.grey[400]! : Colors.transparent,
-            shape: BoxShape.circle,
-          ),
-          child: 
-              ? Lottie.asset((timeUp == false) ? "assets/json/fail.json" : assets/json/time-up.json)
-              : Image.asset("assets/images/referee.png"),
-        ),
+            padding: EdgeInsets.all(35.sp),
+            decoration: BoxDecoration(
+              color: timeUp ? Colors.grey[400]! : Colors.transparent,
+              shape: BoxShape.circle,
+            ),
+            child: (timeUp == false) ?  Lottie.asset(
+                 "assets/json/fail.json")),
         SizedBox(height: 20.h),
         Text(
           (timeUp == false) ? dialog["title"] : "Time's up!",
