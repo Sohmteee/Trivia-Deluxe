6a92cee0833436cc1165b034a30311fb65642086
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-04 08:34:28 +0100

    commit

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -5,7 +5,6 @@ List riddlesData = [
     "question":
         "What is there one of in every corner and two of in every room?",
     "dialog": {
-      "image": "assets/images/letter-o.png",
       "title": "The letter 'O'",
       "content":
           "'O' appears once in the word 'corner' and twice in the word 'room'",
@@ -36,7 +35,6 @@ List riddlesData = [
   {
     "question": "What is stronger than steel but can't handle the sun?",
     "dialog": {
-      "image": "assets/images/ice-cubes.png",
       "title": "Ice",
       "content": "Ice is strong but ironically melts under the sun",
     },
@@ -66,7 +64,6 @@ List riddlesData = [
   {
     "question": "The more there is, the less you see. What am I?",
     "dialog": {
-      "image": "assets/images/night-mode.png",
       "title": "Darkness",
       "content": "You see less the darker it gets",
     },
@@ -96,7 +93,6 @@ List riddlesData = [
   {
     "question": "I am full of holes but strong as steel. What am I?",
     "dialog": {
-      "image": "assets/images/chain.png",
       "title": "A chain",
       "content": "A chain can hold up to 125,000 pounds of weight",
     },
@@ -126,7 +122,6 @@ List riddlesData = [
   {
     "question": "You are my brother, but I'm not your brother. Who am I?",
     "dialog": {
-      "image": "assets/images/siblings.png",
       "title": "I'm your sister",
       "content": "Sometimes they're annoying, but it's great to have them.",
     },
@@ -156,7 +151,6 @@ List riddlesData = [
   {
     "question": "What can fill an entire room without taking up any space?",
     "dialog": {
-      "image": "assets/images/flashlight.png",
       "title": "Light",
       "content": "Light has no mass, neither does it occupy space",
     },
@@ -186,7 +180,6 @@ List riddlesData = [
   {
     "question": "What has 10 letters and starts with gas?",
     "dialog": {
-      "image": "assets/images/refuel.png",
       "title": "Automobile",
       "content": "An automobile has 10 letters and starts with gas (fuel)",
     },
@@ -216,7 +209,6 @@ List riddlesData = [
   {
     "question": "What is it that no-one wants but no-one wants to lose?",
     "dialog": {
-      "image": "assets/images/judge.png",
       "title": "Lawsuit",
       "content": "Who would want to lose a lawsuit?",
     },
@@ -247,7 +239,6 @@ List riddlesData = [
     "question": "I am an odd number. Take away a letter an I become even. "
         "What am I?",
     "dialog": {
-      "image": "assets/images/seven.png",
       "title": "Seven",
       "content": "Removing 's' from 'seven' makes it 'even'",
     },
@@ -277,7 +268,6 @@ List riddlesData = [
   {
     "question": "Who has married many women but was never married?",
     "dialog": {
-      "image": "assets/images/priest.png",
       "title": "A Priest",
       "content": "A Preist never gets married",
     },
@@ -310,7 +300,6 @@ List animalsData = [
   {
     "question": "How many years can a snail sleep for?",
     "dialog": {
-      "image": "assets/images/snail.png",
       "title": "3 years",
       "content": "Snails can sleep up to 3 years at a time",
     },
@@ -340,7 +329,6 @@ List animalsData = [
   {
     "question": "Which animal has no vocal cords?",
     "dialog": {
-      "image": "assets/images/giraffe.png",
       "title": "Giraffes",
       "content": "While they do have a vocal box, giraffes have no vocal cords",
     },
@@ -370,7 +358,6 @@ List animalsData = [
   {
     "question": "How many eyes does a honey bee have?",
     "dialog": {
-      "image": "assets/images/bee.png",
       "title": "5 eyes",
       "content": "The honey bee has 2 large compound eyes and 3 smaller eyes "
           "in the center of their head, for a total of 5 eyes",
@@ -401,7 +388,6 @@ List animalsData = [
   {
     "question": "What is a female donkey called?",
     "dialog": {
-      "image": "assets/images/donkey.png",
       "title": "A Jenny",
       "content": "Female donkeys are called a jenny or jennet",
     },
@@ -431,7 +417,6 @@ List animalsData = [
   {
     "question": "How many hearts does an octopus have?",
     "dialog": {
-      "image": "assets/images/octopus.png",
       "title": "3 hearts",
       "content": "An octopus has 3 hearts, one to circulate blood and two "
           "to pump it passed its gills and pick up oxygen.",
@@ -462,7 +447,6 @@ List animalsData = [
   {
     "question": "What type of animal is a flemish giant?",
     "dialog": {
-      "image": "assets/images/rabbit.png",
       "title": "A rabbit",
       "content":
           "The Flemish Giant is one of the largest breeds of a domestic rabbit.",
@@ -493,7 +477,6 @@ List animalsData = [
   {
     "question": "How long does it take for a sloth to digest its food?",
     "dialog": {
-      "image": "assets/images/sloth.png",
       "title": "Two weeks",
       "content":
           "Sloths are the mammals that take the longest to digest their meals.",
@@ -524,7 +507,6 @@ List animalsData = [
   {
     "question": "How many bones does a shark have?",
     "dialog": {
-      "image": "assets/images/shark.png",
       "title": "O bones!",
       "content": "Sharks have no bones, but have cartilaginous tissue instead.",
     },
@@ -554,7 +536,6 @@ List animalsData = [
   {
     "question": "What color is the beak of a mature mute swan?",
     "dialog": {
-      "image": "assets/images/swan.png",
       "title": "Orange",
       "content": "Mute Swans are known for their white bodies with an orange "
           "beak that has a black base",
@@ -585,7 +566,6 @@ List animalsData = [
   {
     "question": "Which animal has the largest eyes in the animal kingdom?",
     "dialog": {
-      "image": "assets/images/squid.png",
       "title": "The Giant Squid",
       "content":
           "The ostrich is the land animal with largest eyes, with a diameter of 2 inches. "
@@ -620,7 +600,6 @@ List proverbsData = [
   {
     "question": "Beggars can't be ___.",
     "dialog": {
-      "image": "assets/images/beggar.png",
       "title": "Beggars can't be choosers",
       "content":
           "People dependent on others must be content with what is offered to them",
@@ -651,7 +630,6 @@ List proverbsData = [
   {
     "question": "A bird in the hand is worth two in the ___.",
     "dialog": {
-      "image": "assets/images/dead-bird.png",
       "title": "A bird in the hand is worth two in the bush",
       "content": "What you have is better than what you might get",
     },
@@ -681,7 +659,6 @@ List proverbsData = [
   {
     "question": "The grass is always ___ on the other side.",
     "dialog": {
-      "image": "assets/images/grass.png",
       "title": " The grass is always greener on the other side",
       "content":
           "Other people always seem to be in a better situation, although it might not be true",
@@ -712,7 +689,6 @@ List proverbsData = [
   {
     "question": "Every cloud has a ___ lining",
     "dialog": {
-      "image": "assets/images/cloud.png",
       "title": "Every cloud has a silver lining",
       "content": "There is always a positive side to any difficult situation",
     },
@@ -742,7 +718,6 @@ List proverbsData = [
   {
     "question": "Out of sight, out of ___",
     "dialog": {
-      "image": "assets/images/close.png",
       "title": "Out of sight, out of mind",
       "content":
           "People or things that are no longer present are easily forgotten",
@@ -773,7 +748,6 @@ List proverbsData = [
   {
     "question": "Good things come to those who ___",
     "dialog": {
-      "image": "assets/images/patience.png",
       "title": "Good things come to those who wait",
       "content": "Patience is rewarded",
     },
@@ -803,7 +777,6 @@ List proverbsData = [
   {
     "question": "A drowning man will clutch at a ___",
     "dialog": {
-      "image": "assets/images/drown.png",
       "title": "A drowning man will clutch at a straw",
       "content":
           "If someone is in a difficult or tough situation, he/ she will grasp on any available opportunity",
@@ -834,7 +807,6 @@ List proverbsData = [
   {
     "question": "Practice what you ___",
     "dialog": {
-      "image": "assets/images/pulpit.png",
       "title": " Practice what you preach",
       "content": "Behave the way you encourage other people to behave",
     },
@@ -864,7 +836,6 @@ List proverbsData = [
   {
     "question": "Ignorance is ___",
     "dialog": {
-      "image": "assets/images/ignore.png",
       "title": "Ignorance is bliss",
       "content": "If you don’t know about something, you don’t need to bother "
           "about it. In other words, if you’re unaware of something, it won’t cause "
@@ -896,7 +867,6 @@ List proverbsData = [
   {
     "question": " No news is ___ news",
     "dialog": {
-      "image": "assets/images/good-news.png",
       "title": " No news is good news",
       "content": "If you don’t receive any news about someone or something, "
           "it means that everything is fine and going normally.",
