2558a04ac4d56e9045eb8aed11fb87777248dd1b
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-05 04:31:15 +0100

    commit

--- lib/models/dialogs/credits.dart
+++ lib/models/dialogs/credits.dart
@@ -1,1 +1,10 @@
-showGameD
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:trivia/models/dialogs/game.dart';
+
+showCreditsDialog(BuildContext context) {
+  showGameDialog(
+    context,
+    isExitable: true,
+    padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 40.h), child: child)
+}
