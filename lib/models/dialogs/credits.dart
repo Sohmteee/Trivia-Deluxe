3ad32926063f0b393238982cb60058d41fb9362d
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-05 04:32:28 +0100

    commit

--- lib/models/dialogs/credits.dart
+++ lib/models/dialogs/credits.dart
@@ -6,5 +6,20 @@ showCreditsDialog(BuildContext context) {
   showGameDialog(
     context,
     isExitable: true,
-    padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 40.h), child: child)
+    // padding: EdgeInsets.symmetric(horizontal: 20.w, vertical: 40.h),
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [  
+        Text(
+          "Credits",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.sp,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    ),
+  );
 }
