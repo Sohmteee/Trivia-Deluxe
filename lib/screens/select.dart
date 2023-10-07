46910738cfa31a2ca34fbe0b82d73edf65bc1c76
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-07 14:47:58 +0100

    commit

--- lib/screens/select.dart
+++ lib/screens/select.dart
@@ -37,9 +37,11 @@ class _SelectScreenState extends State<SelectScreen> {
 
   @override
   void initState() {
-for (var item in selectItems){
-  if (item["data"]["title"])
-}
+    for (var item in selectItems) {
+      if (item["data"]["title"] == null) {
+        box.put(item["data"]["title"], item["data"]);
+      }
+    }
 
     for (var item in selectItems) {
       print("Level: ${box.get(item["data"]["title"])?["currentLevel"]}");
