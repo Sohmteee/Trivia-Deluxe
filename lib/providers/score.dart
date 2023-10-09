1e5360046734675687482dd5532bdd9fd4fab300
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-09 10:10:15 +0100

    commit

--- lib/providers/score.dart
+++ lib/providers/score.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:trivia/data/box.dart';
 
 class ScoreProvider extends ChangeNotifier {
-  int _score = 0;
+  int _score = box.get("score") ?? 0;
   int get score => _score;
 
   void updateScore(int newScore) {
     _score += newScore;
+    box.put("score", score);
     debugPrint("Curent Score: $score");
     notifyListeners();
   }
 
   void resetScore() {
     _score = 0;
+    box.put("score", score);
   }
 }
