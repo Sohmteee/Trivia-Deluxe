be0d807c830f2d19c209d77af12b43d9dabdc7cb
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-14 11:51:59 +0100

    commit

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -346,6 +346,7 @@ class _MenuScreenState extends State<MenuScreen>
         },
         onAdFailedToLoad: (ad, err) {
           print('Failed to load a banner ad: ${err.message}');
+          Toast.show(err.message, duration: , gravity: Toast.bottom);
           ad.dispose();
         },
       ),
