5b27de5d8a5ed3ef3876891cf9a545089ce79c0b
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-18 15:06:36 +0100

    commit

--- lib/providers/stage.dart
+++ lib/providers/stage.dart
@@ -20,30 +20,34 @@ class StageProvider extends ChangeNotifier {
 /* 
 Achievements
 
+Level Achievements
 1. Complete 5 levels without failing: "Steady Progress"
 2. Complete 10 levels without failing: "Leveling Up"
 3. Complete 20 levels without failing: "Tenacious Triumph"
 4. Complete 50 levels without failing: "Masterful Streak"
 5. Complete 100 levels without failing: "Unstoppable Champion"
 
+Coin Achievements
 6. Earn 50 coins: "Coin Collector"
 7. Earn 100 coins: "Coin Hoarder"
 8. Earn 200 coins: "Gold Gatherer"
 9. Earn 500 coins: "Treasure Hunter"
 10. Earn 1000 coins: "Coin Millionaire"
 
+Trivia Achievements
 11. Answer 5 questions correctly in a row: "Trivia Beginner"
 12. Answer 10 questions correctly in a row: "Trivia Expert"
 13. Answer 20 questions correctly in a row: "Brainiac"
 14. Answer 50 questions correctly in a row: "Trivia Grandmaster"
 15. Answer 100 questions correctly in a row: "Trivia Maestro"
 
-
+Leaderboard Achievements
 16. Top 10 in leaderboard: "Achiever"
 17. Top 5 in leaderboard: "High Achiever"
 18. Top 3 in leaderboard: "Top Ranker"
 19. First in leaderboard: "Leaderboard Champion"
 
+Ultimate Achievement
 20. Collect all achievements: "Ultimate Achiever"
 
 
