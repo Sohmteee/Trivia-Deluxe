bbf6ec19876b5bf25613588290cfa8c13610e2c4
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-23 16:58:15 +0100

    commit

--- lib/providers/streaks.dart
+++ lib/providers/streaks.dart
@@ -1,119 +1,124 @@
 import 'package:flutter/material.dart';
 
 Map streaks = {
-  
-  {"streaks": [
-    {
-      "Complete 5 levels without failing": {
-        "status": false,
-        "progress": 0,
-        "limit": 5,
-      }
-    },
-    {
-      "Complete 10 levels without failing": {
-        "status": false,
-        "progress": 0,
-        "limit": 10,
-      }
-    },
-    {
-      "Complete 20 levels without failing": {
-        "status": false,
-        "progress": 0,
-        "limit": 20,
-      }
-    },
-    {
-      "Complete 50 levels without failing": {
-        "status": false,
-        "progress": 0,
-        "limit": 50,
-      }
-    },
-    {
-      "Complete 100 levels without failing": {
-        "status": false,
-        "progress": 0,
-        "limit": 100,
-      }
-    },
-  ],}
-  "Coin Streaks": [
-    {
-      "Earn 50 coins": {
-        "status": false,
-        "progress": 0,
-        "limit": 50,
-      }
-    },
-    {
-      "Earn 100 coins": {
-        "status": false,
-        "progress": 0,
-        "limit": 100,
-      }
-    },
-    {
-      "Earn 200 coins": {
-        "status": false,
-        "progress": 0,
-        "limit": 200,
-      }
-    },
-    {
-      "Earn 500 coins": {
-        "status": false,
-        "progress": 0,
-        "limit": 500,
-      }
-    },
-    {
-      "Earn 1000 coins": {
-        "status": false,
-        "progress": 0,
-        "limit": 1000,
-      }
-    },
-  ],
+  "Level Streaks": {
+    "image": "",
+    "streaks": [
+      {
+        "Complete 5 levels without failing": {
+          "status": false,
+          "progress": 0,
+          "limit": 5,
+        }
+      },
+      {
+        "Complete 10 levels without failing": {
+          "status": false,
+          "progress": 0,
+          "limit": 10,
+        }
+      },
+      {
+        "Complete 20 levels without failing": {
+          "status": false,
+          "progress": 0,
+          "limit": 20,
+        }
+      },
+      {
+        "Complete 50 levels without failing": {
+          "status": false,
+          "progress": 0,
+          "limit": 50,
+        }
+      },
+      {
+        "Complete 100 levels without failing": {
+          "status": false,
+          "progress": 0,
+          "limit": 100,
+        }
+      },
+    ],
+  },
+  "Coin Streaks": {
+    "streaks": [
+      {
+        "Earn 50 coins": {
+          "status": false,
+          "progress": 0,
+          "limit": 50,
+        }
+      },
+      {
+        "Earn 100 coins": {
+          "status": false,
+          "progress": 0,
+          "limit": 100,
+        }
+      },
+      {
+        "Earn 200 coins": {
+          "status": false,
+          "progress": 0,
+          "limit": 200,
+        }
+      },
+      {
+        "Earn 500 coins": {
+          "status": false,
+          "progress": 0,
+          "limit": 500,
+        }
+      },
+      {
+        "Earn 1000 coins": {
+          "status": false,
+          "progress": 0,
+          "limit": 1000,
+        }
+      },
+    ],
+  },
   "Trivia Streaks": {
-    
-    {
-      "Answer 5 questions correctly in a row": {
-        "status": false,
-        "progress": 0,
-        "limit": 5,
-      }
+    "streaks": {
+      {
+        "Answer 5 questions correctly in a row": {
+          "status": false,
+          "progress": 0,
+          "limit": 5,
+        }
+      },
+      {
+        "Answer 10 questions correctly in a row": {
+          "status": false,
+          "progress": 0,
+          "limit": 10,
+        }
+      },
+      {
+        "Answer 20 questions correctly in a row": {
+          "status": false,
+          "progress": 0,
+          "limit": 20,
+        }
+      },
+      {
+        "Answer 50 questions correctly in a row": {
+          "status": false,
+          "progress": 0,
+          "limit": 50,
+        }
+      },
+      {
+        "Answer 100 questions correctly in a row": {
+          "status": false,
+          "progress": 0,
+          "limit": 100,
+        }
+      },
     },
-    {
-      "Answer 10 questions correctly in a row": {
-        "status": false,
-        "progress": 0,
-        "limit": 10,
-      }
-    },
-    {
-      "Answer 20 questions correctly in a row": {
-        "status": false,
-        "progress": 0,
-        "limit": 20,
-      }
-    },
-    {
-      "Answer 50 questions correctly in a row": {
-        "status": false,
-        "progress": 0,
-        "limit": 50,
-      }
-    },
-    {
-      "Answer 100 questions correctly in a row": {
-        "status": false,
-        "progress": 0,
-        "limit": 100,
-      }
-    },
-  }
+  },
 };
 
 class StreaksProvider extends ChangeNotifier {}
