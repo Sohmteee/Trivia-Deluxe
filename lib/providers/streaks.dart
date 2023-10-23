aafcdf26008616a6dc7c1d02d3976e7df5fce367
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-23 19:23:51 +0100

    commit

--- lib/providers/streaks.dart
+++ lib/providers/streaks.dart
@@ -2,184 +2,189 @@ import 'package:flutter/material.dart';
 import 'package:trivia/data/box.dart';
 
 class StreaksProvider extends ChangeNotifier {
-  List<Map<String, dynamic>> streaks = box.get("streaks") ?? [
-    {
-      "title": "Level Streaks",
-      "image": "assets/images/level.png",
-      "streaks": [
+  List<Map<String, dynamic>> streaks = box.get("streaks") ??
+      [
         {
-          "title": "Steady Progress",
-          "subtitle": "Complete 5 levels without failing",
-          "status": false,
-          "progress": 0,
-          "limit": 5,
+          "title": "Level Streaks",
+          "image": "assets/images/level.png",
+          "streaks": [
+            {
+              "title": "Steady Progress",
+              "subtitle": "Complete 5 levels without failing",
+              "status": false,
+              "progress": 0,
+              "limit": 5,
+            },
+            {
+              "title": "Leveling Up",
+              "subtitle": "Complete 10 levels without failing",
+              "status": false,
+              "progress": 0,
+              "limit": 10,
+            },
+            {
+              "title": "Tenacious Triumph",
+              "subtitle": "Complete 20 levels without failing",
+              "status": false,
+              "progress": 0,
+              "limit": 20,
+            },
+            {
+              "title": "Masterful Streak",
+              "subtitle": "Complete 50 levels without failing",
+              "status": false,
+              "progress": 0,
+              "limit": 50,
+            },
+            {
+              "title": "Unstoppable Champion",
+              "subtitle": "Complete 100 levels without failing",
+              "status": false,
+              "progress": 0,
+              "limit": 100,
+            },
+          ],
         },
         {
-          "title": "Leveling Up",
-          "subtitle": "Complete 10 levels without failing",
-          "status": false,
-          "progress": 0,
-          "limit": 10,
+          "title": "Coin Streaks",
+          "image": "assets/images/coins.png",
+          "streaks": [
+            {
+              "title": "Coin Collector",
+              "subtitle": "Earn 50 coins",
+              "status": false,
+              "progress": 0,
+              "limit": 50,
+            },
+            {
+              "title": "Coin Hoarder",
+              "subtitle": "Earn 100 coins",
+              "status": false,
+              "progress": 0,
+              "limit": 100,
+            },
+            {
+              "title": "Gold Gatherer",
+              "subtitle": "Earn 200 coins",
+              "status": false,
+              "progress": 0,
+              "limit": 200,
+            },
+            {
+              "title": "Treasure Hunter",
+              "subtitle": "Earn 500 coins",
+              "status": false,
+              "progress": 0,
+              "limit": 500,
+            },
+            {
+              "title": "Coin Millionaire",
+              "subtitle": "Earn 1000 coins",
+              "status": false,
+              "progress": 0,
+              "limit": 1000,
+            },
+          ],
         },
         {
-          "title": "Tenacious Triumph",
-          "subtitle": "Complete 20 levels without failing",
-          "status": false,
-          "progress": 0,
-          "limit": 20,
+          "title": "Trivia Streaks",
+          "image": "assets/images/answer.png",
+          "streaks": [
+            {
+              "title": "Trivia Beginner",
+              "subtitle": "Answer 5 questions correctly in a row",
+              "status": false,
+              "progress": 0,
+              "limit": 5,
+            },
+            {
+              "title": "Trivia Expert",
+              "subtitle": "Answer 10 questions correctly in a row",
+              "status": false,
+              "progress": 0,
+              "limit": 10,
+            },
+            {
+              "title": "Brainiac",
+              "subtitle": "Answer 20 questions correctly in a row",
+              "status": false,
+              "progress": 0,
+              "limit": 20,
+            },
+            {
+              "title": "Trivia Grandmaster",
+              "subtitle": "Answer 50 questions correctly in a row",
+              "status": false,
+              "progress": 0,
+              "limit": 50,
+            },
+            {
+              "title": "Trivia Maestro",
+              "subtitle": "Answer 100 questions correctly in a row",
+              "status": false,
+              "progress": 0,
+              "limit": 100,
+            },
+          ],
         },
         {
-          "title": "Masterful Streak",
-          "subtitle": "Complete 50 levels without failing",
-          "status": false,
-          "progress": 0,
-          "limit": 50,
+          "title": "Leaderboard Streaks",
+          "image": "assets/images/rank.png",
+          "streaks": [
+            {
+              "title": "Achiever",
+              "subtitle": "Rank top 10 on the leaderboard",
+              "status": false,
+              "progress": 0,
+              "limit": 1,
+            },
+            {
+              "title": "Top Ranker",
+              "subtitle": "Rank top 5 on the leaderboard",
+              "status": false,
+              "progress": 0,
+              "limit": 1,
+            },
+            {
+              "title": "Master Ranker",
+              "subtitle": "Rank top 3 on the leaderboard",
+              "status": false,
+              "progress": 0,
+              "limit": 1,
+            },
+            {
+              "title": "Leaderboard Champion",
+              "subtitle": "Rank first on the leaderboard",
+              "status": false,
+              "progress": 0,
+              "limit": 1,
+            },
+          ],
         },
         {
-          "title": "Unstoppable Champion",
-          "subtitle": "Complete 100 levels without failing",
-          "status": false,
-          "progress": 0,
-          "limit": 100,
+          "title": "Ultimate Achievement",
+          "image": "assets/images/goal.png",
+          "streaks": [
+            {
+              "title": "Ultimate Achiever",
+              "subtitle": "Finish all streaks",
+              "status": false,
+              "progress": 0,
+              "limit": 19,
+            },
+          ],
         },
-      ],
-    },
-    {
-      "title": "Coin Streaks",
-      "image": "assets/images/coins.png",
-      "streaks": [
-        {
-          "title": "Coin Collector",
-          "subtitle": "Earn 50 coins",
-          "status": false,
-          "progress": 0,
-          "limit": 50,
-        },
-        {
-          "title": "Coin Hoarder",
-          "subtitle": "Earn 100 coins",
-          "status": false,
-          "progress": 0,
-          "limit": 100,
-        },
-        {
-          "title": "Gold Gatherer",
-          "subtitle": "Earn 200 coins",
-          "status": false,
-          "progress": 0,
-          "limit": 200,
-        },
-        {
-          "title": "Treasure Hunter",
-          "subtitle": "Earn 500 coins",
-          "status": false,
-          "progress": 0,
-          "limit": 500,
-        },
-        {
-          "title": "Coin Millionaire",
-          "subtitle": "Earn 1000 coins",
-          "status": false,
-          "progress": 0,
-          "limit": 1000,
-        },
-      ],
-    },
-    {
-      "title": "Trivia Streaks",
-      "image": "assets/images/answer.png",
-      "streaks": [
-        {
-          "title": "Trivia Beginner",
-          "subtitle": "Answer 5 questions correctly in a row",
-          "status": false,
-          "progress": 0,
-          "limit": 5,
-        },
-        {
-          "title": "Trivia Expert",
-          "subtitle": "Answer 10 questions correctly in a row",
-          "status": false,
-          "progress": 0,
-          "limit": 10,
-        },
-        {
-          "title": "Brainiac",
-          "subtitle": "Answer 20 questions correctly in a row",
-          "status": false,
-          "progress": 0,
-          "limit": 20,
-        },
-        {
-          "title": "Trivia Grandmaster",
-          "subtitle": "Answer 50 questions correctly in a row",
-          "status": false,
-          "progress": 0,
-          "limit": 50,
-        },
-        {
-          "title": "Trivia Maestro",
-          "subtitle": "Answer 100 questions correctly in a row",
-          "status": false,
-          "progress": 0,
-          "limit": 100,
-        },
-      ],
-    },
-    {
-      "title": "Leaderboard Streaks",
-      "image": "assets/images/rank.png",
-      "streaks": [
-        {
-          "title": "Achiever",
-          "subtitle": "Rank top 10 on the leaderboard",
-          "status": false,
-          "progress": 0,
-          "limit": 1,
-        },
-        {
-          "title": "Top Ranker",
-          "subtitle": "Rank top 5 on the leaderboard",
-          "status": false,
-          "progress": 0,
-          "limit": 1,
-        },
-        {
-          "title": "Master Ranker",
-          "subtitle": "Rank top 3 on the leaderboard",
-          "status": false,
-          "progress": 0,
-          "limit": 1,
-        },
-        {
-          "title": "Leaderboard Champion",
-          "subtitle": "Rank first on the leaderboard",
-          "status": false,
-          "progress": 0,
-          "limit": 1,
-        },
-      ],
-    },
-    {
-      "title": "Ultimate Achievement",
-      "image": "assets/images/goal.png",
-      "streaks": [
-        {
-          "title": "Ultimate Achiever",
-          "subtitle": "Finish all streaks",
-          "status": false,
-          "progress": 0,
-          "limit": 19,
-        },
-      ],
-    },
-  ];
+      ];
 
-  int levelStreak = 0, permanentLevelStreak = 0;
-  int coinStreak = 0, permanentCoinStreak = 0;
-  int triviaStreak = 0, permanentTriviaStreak = 0;
-  int leaderboardStreak = 0, permanentLeaderboardStreak = 0;
-  int ultimateStreak = 0;
+  int levelStreak = box.get("levelStreak") ?? 0,
+      permanentLevelStreak = box.get("permanentLevelStreak") ?? 0;
+  int coinStreak = box.get("coinStreak") ?? 0,
+      permanentCoinStreak = box.get("permanentCoinStreak") ?? 0;
+  int triviaStreak = box.get("triviaStreak") ?? 0,
+      permanentTriviaStreak = box.get("permanentTriviaStreak") ?? 0;
+  int leaderboardStreak = box.get("leaderboardStreak") ?? 0,
+      permanentLeaderboardStreak = box.get("permanentLeaderboardStreak") ?? 0;
+  int ultimateStreak = box.get("ultimateStreak") ?? 0;
 
   void updateStreakStatus(int index, int streakIndex) {
     streaks[index]["streaks"][streakIndex]["status"] = true;
