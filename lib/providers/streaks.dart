547d40fb8e2319c27495926993693a68603caaed
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-23 16:22:02 +0100

    commit

--- lib/providers/streaks.dart
+++ lib/providers/streaks.dart
@@ -8,7 +8,35 @@ Map streaks = {
         "progress": 0,
         "limit": 5,
       }
-    }
+    },
+    {
+      "Complete 10 levels without failing": {
+        "status": false,
+        "progress": 0,
+        "limit": 10,
+      }
+    },
+    {
+      "Complete 20 levels without failing": {
+        "status": false,
+        "progress": 0,
+        "limit": 20,
+      }
+    },
+    {
+      "Complete 50 levels without failing": {
+        "status": false,
+        "progress": 0,
+        "limit": 50,
+      }
+    },
+    {
+      "Complete 100 levels without failing": {
+        "status": false,
+        "progress": 0,
+        "limit": 100,
+      }
+    },
   ],
 };
 
