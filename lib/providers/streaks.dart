2289ac1227979785e80155714b342587b48464dd
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-23 23:37:29 +0100

    commit

--- lib/providers/streaks.dart
+++ lib/providers/streaks.dart
@@ -206,35 +206,55 @@ class StreaksProvider extends ChangeNotifier {
             "title": "Steady Progress",
             "subtitle": "Complete 5 levels without failing",
             "status": permanentLevelStreak >= 5,
-            "progress": permanentLevelStreak >= 5 ? 5 : levelStreak >= 5 ? 5 : levelStreak,
+            "progress": permanentLevelStreak >= 5
+                ? 5
+                : levelStreak >= 5
+                    ? 5
+                    : levelStreak,
             "limit": 5,
           },
           {
             "title": "Leveling Up",
             "subtitle": "Complete 10 levels without failing",
             "status": permanentLevelStreak >= 10,
-            "progress": permanentLevelStreak >= 10 ? 10 : levelStreak >= 10 ? 10 : levelStreak,
+            "progress": permanentLevelStreak >= 10
+                ? 10
+                : levelStreak >= 10
+                    ? 10
+                    : levelStreak,
             "limit": 10,
           },
           {
             "title": "Tenacious Triumph",
             "subtitle": "Complete 20 levels without failing",
             "status": permanentLevelStreak >= 20,
-            "progress": permanentLevelStreak >= 20 ? 20 : levelStreak >= 20 ? 20 : levelStreak,
+            "progress": permanentLevelStreak >= 20
+                ? 20
+                : levelStreak >= 20
+                    ? 20
+                    : levelStreak,
             "limit": 20,
           },
           {
             "title": "Masterful Streak",
             "subtitle": "Complete 50 levels without failing",
             "status": permanentLevelStreak >= 50,
-            "progress": permanentLevelStreak >= 50 ? 50 : levelStreak >= 50 ? 50 : levelStreak,
+            "progress": permanentLevelStreak >= 50
+                ? 50
+                : levelStreak >= 50
+                    ? 50
+                    : levelStreak,
             "limit": 50,
           },
           {
             "title": "Unstoppable Champion",
             "subtitle": "Complete 100 levels without failing",
             "status": permanentLevelStreak >= 100,
-            "progress": permanentLevelStreak >= 100 ? 100 : levelStreak >= 100 ? 100 : levelStreak,
+            "progress": permanentLevelStreak >= 100
+                ? 100
+                : levelStreak >= 100
+                    ? 100
+                    : levelStreak,
             "limit": 100,
           },
         ],
@@ -247,35 +267,55 @@ class StreaksProvider extends ChangeNotifier {
             "title": "Coin Collector",
             "subtitle": "Earn 50 coins",
             "status": permanentCoinStreak >= 50,
-            "progress": permanentCoinStreak >= 50 ? 50 : coinStreak >= 50 ? 50 : coinStreak,
+            "progress": permanentCoinStreak >= 50
+                ? 50
+                : coinStreak >= 50
+                    ? 50
+                    : coinStreak,
             "limit": 50,
           },
           {
             "title": "Coin Hoarder",
             "subtitle": "Earn 100 coins",
             "status": permanentCoinStreak >= 100,
-            "progress": permanentCoinStreak >= 100 ? 100 : coinStreak >= 100 ? 100 : coinStreak,
+            "progress": permanentCoinStreak >= 100
+                ? 100
+                : coinStreak >= 100
+                    ? 100
+                    : coinStreak,
             "limit": 100,
           },
           {
             "title": "Gold Gatherer",
             "subtitle": "Earn 200 coins",
             "status": permanentCoinStreak >= 200,
-            "progress": permanentCoinStreak >= 200 ? 200 : coinStreak >= 200 ? 200 : coinStreak,
+            "progress": permanentCoinStreak >= 200
+                ? 200
+                : coinStreak >= 200
+                    ? 200
+                    : coinStreak,
             "limit": 200,
           },
           {
             "title": "Treasure Hunter",
             "subtitle": "Earn 500 coins",
             "status": permanentCoinStreak >= 500,
-            "progress": permanentCoinStreak >= 500 ? 500 : coinStreak >= 500 ? 500 : coinStreak,
+            "progress": permanentCoinStreak >= 500
+                ? 500
+                : coinStreak >= 500
+                    ? 500
+                    : coinStreak,
             "limit": 500,
           },
           {
             "title": "Coin Millionaire",
             "subtitle": "Earn 1000 coins",
             "status": permanentCoinStreak >= 1000,
-            "progress": permanentCoinStreak >= 1000 ? 1000 : coinStreak >= 1000 ? 1000 : coinStreak,
+            "progress": permanentCoinStreak >= 1000
+                ? 1000
+                : coinStreak >= 1000
+                    ? 1000
+                    : coinStreak,
             "limit": 1000,
           },
         ],
@@ -288,35 +328,55 @@ class StreaksProvider extends ChangeNotifier {
             "title": "Trivia Beginner",
             "subtitle": "Answer 5 questions correctly in a row",
             "status": permanentTriviaStreak >= 5,
-            "progress": permanentTriviaStreak >= 5 ? 5 : triviaStreak >= 5 ? 5 : triviaStreak,
+            "progress": permanentTriviaStreak >= 5
+                ? 5
+                : triviaStreak >= 5
+                    ? 5
+                    : triviaStreak,
             "limit": 5,
           },
           {
             "title": "Trivia Expert",
             "subtitle": "Answer 10 questions correctly in a row",
             "status": permanentTriviaStreak >= 10,
-            "progress": permanentTriviaStreak >= 10 ? 10 : triviaStreak >= 10 ? 10 : triviaStreak,
+            "progress": permanentTriviaStreak >= 10
+                ? 10
+                : triviaStreak >= 10
+                    ? 10
+                    : triviaStreak,
             "limit": 10,
           },
           {
             "title": "Brainiac",
             "subtitle": "Answer 20 questions correctly in a row",
             "status": permanentTriviaStreak >= 20,
-            "progress": permanentTriviaStreak >= 20 ? 20 : triviaStreak >= 20 ? 20 : triviaStreak,
+            "progress": permanentTriviaStreak >= 20
+                ? 20
+                : triviaStreak >= 20
+                    ? 20
+                    : triviaStreak,
             "limit": 20,
           },
           {
             "title": "Trivia Grandmaster",
             "subtitle": "Answer 50 questions correctly in a row",
             "status": permanentTriviaStreak >= 50,
-            "progress": permanentTriviaStreak >= 50 ? 50 : triviaStreak >= 50 ? 50 : triviaStreak,
+            "progress": permanentTriviaStreak >= 50
+                ? 50
+                : triviaStreak >= 50
+                    ? 50
+                    : triviaStreak,
             "limit": 50,
           },
           {
             "title": "Trivia Maestro",
             "subtitle": "Answer 100 questions correctly in a row",
             "status": permanentTriviaStreak >= 100,
-            "progress": permanentTriviaStreak >= 100 ? 100 : triviaStreak >= 100 ? 100 : triviaStreak,
+            "progress": permanentTriviaStreak >= 100
+                ? 100
+                : triviaStreak >= 100
+                    ? 100
+                    : triviaStreak,
             "limit": 100,
           },
         ],
@@ -379,7 +439,11 @@ class StreaksProvider extends ChangeNotifier {
             "title": "Ultimate Achiever",
             "subtitle": "Finish all streaks",
             "status": ultimateStreak >= 19,
-            "progress": ultimateStreak >= 19 ? 19 : ultimateStreak >= 19 ? 19 : ultimateStreak,
+            "progress": ultimateStreak >= 19
+                ? 19
+                : ultimateStreak >= 19
+                    ? 19
+                    : ultimateStreak,
             "limit": 19,
           },
         ],
@@ -417,6 +481,7 @@ class StreaksProvider extends ChangeNotifier {
         permanentCoinStreakList.contains(coinStreak)) {
       permanentCoinStreak = coinStreak;
       box.put("permanentCoinStreak", permanentCoinStreak);
+      print("permanentCoinStreak updated to $permanentCoinStreak");
     }
 
     updateUltimateStreak();
