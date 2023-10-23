edb65924950335462d2fe26a990ce34c8fd36b72
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-23 15:35:15 +0100

    commit

--- lib/screens/streaks.dart
+++ lib/screens/streaks.dart
@@ -14,16 +14,30 @@ class _StreaksScreeenState extends State<StreaksScreeen> {
   @override
   Widget build(BuildContext context) {
     return GameBackground(
-      body: Column(
-        children: [
-          Text(
-            "Leaderboard",
-            style: TextStyle(
-              color: AppColor.yellow,
-              fontSize: 30.sp,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 40.w, vertical: 40.h),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                "Leaderboard",
+                style: TextStyle(
+                  color: AppColor.yellow,
+                  fontSize: 50.sp,
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-        ],
+            Text(
+              "This feature is coming soon",
+              style: TextStyle(
+                color: AppColor.white,
+                fontSize: 20.sp,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
