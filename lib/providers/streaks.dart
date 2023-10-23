7dbe7b6315c2bae0b7442a317d4fff1ed2d1f13f
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-23 16:20:40 +0100

    commit

--- lib/providers/streaks.dart
+++ lib/providers/streaks.dart
@@ -3,15 +3,13 @@ import 'package:flutter/material.dart';
 Map streaks = {
   "Level Streaks": [
     {
-      "Complete 5 levels without failing" : {
+      "Complete 5 levels without failing": {
         "status": false,
         "progress": 0,
         "limit": 5,
-      },
+      }
     }
-  ]
-
-  ,
+  ],
 };
 
 class StreaksProvider extends ChangeNotifier {}
