fb2126fdf9b91cf6613ebdbd8987f01adc07e54d
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-23 18:16:44 +0100

    commit

--- lib/providers/streaks.dart
+++ lib/providers/streaks.dart
@@ -52,44 +52,39 @@ List<Map<String, dynamic>> streaks = [
     "image": "assets/images/coins.png",
     "streaks": [
       {
-        : {
           "title": "Coin Collector","subtitle": "Earn 50 coins",
           "status": false,
           "progress": 0,
           "limit": 50,
-        }
+        
       },
       {
-        : {
           "title": "Coin Hoarder","subtitle": "Earn 100 coins",
           "status": false,
           "progress": 0,
           "limit": 100,
-        }
+        
       },
       {
-        : {
           "title": "Gold Gatherer","subtitle": "Earn 200 coins",
           "status": false,
           "progress": 0,
           "limit": 200,
-        }
+        
       },
       {
-        : {
           "title": "Treasure Hunter","subtitle": "Earn 500 coins",
           "status": false,
           "progress": 0,
           "limit": 500,
-        }
+        
       },
       {
-        : {
           "title": "Coin Millionaire","subtitle": "Earn 1000 coins",
           "status": false,
           "progress": 0,
           "limit": 1000,
-        }
+        
       },
     ],
   },
@@ -98,44 +93,40 @@ List<Map<String, dynamic>> streaks = [
     "image": "assets/images/answer.png",
     "streaks": {
       {
-        : {
           "title": "Trivia Beginner","subtitle": "Answer 5 questions correctly in a row",
           "status": false,
           "progress": 0,
           "limit": 5,
-        }
+        
       },
       {
-        : {
           "title": "Trivia Expert","subtitle": "Answer 10 questions correctly in a row",
           "status": false,
           "progress": 0,
           "limit": 10,
-        }
+        
       },
       {
-: {
           "title":         "Brainiac","subtitle": "Answer 20 questions correctly in a row",
           "status": false,
           "progress": 0,
           "limit": 20,
-        }
+        
       },
       {
-        : {
           "title": "Trivia Grandmaster","subtitle": "Answer 50 questions correctly in a row",
           "status": false,
           "progress": 0,
           "limit": 50,
-        }
+        
       },
       {
-        : {
+        
           "title": "Trivia Maestro","subtitle": "Answer 100 questions correctly in a row",
           "status": false,
           "progress": 0,
           "limit": 100,
-        }
+        
       },
     },
   },
@@ -144,31 +135,31 @@ List<Map<String, dynamic>> streaks = [
     "image": "assets/images/rank.png",
     "streaks": {
       {
-: {
+
           "title":         "Achiever","subtitle": "Rank top 10 on the leaderboard",
           "status": false,
           "progress": 0,
           "limit": 1,
-        },
+        
       },
       {
-        : {
+        
           "title": "Top Ranker","subtitle": "Rank top 5 on the leaderboard",
           "status": false,
           "progress": 0,
           "limit": 1,
-        },
+        
       },
       {
-        : {
+        
           "title": "Master Ranker","subtitle": "Rank top 3 on the leaderboard",
           "status": false,
           "progress": 0,
           "limit": 1,
-        },
+        
       },
       {
-        : {
+        
           "title": "Leaderboard Champion","subtitle": "Rank first on the leaderboard",
           "status": false,
           "progress": 0,
