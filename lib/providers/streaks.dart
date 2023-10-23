787e65e41e2ea89d9925f12db20b60b91d48d884
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-23 16:19:27 +0100

    commit

--- lib/providers/streaks.dart
+++ lib/providers/streaks.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 
 Map streaks = {
   "Level Streaks": [
-    
+    {
+      "Complete 5 levels without failing" : {
+        "status": false,
+        "progress": 0,
+        "limit": 5,
+      },
+    }
   ]
 
   ,
