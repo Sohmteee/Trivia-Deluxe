595b589a2f39fe1f25b7c568328372e9ece921cc
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-23 20:32:35 +0100

    commit

--- lib/providers/streaks.dart
+++ lib/providers/streaks.dart
@@ -202,8 +202,6 @@ class StreaksProvider extends ChangeNotifier {
     levelStreak = level ?? levelStreak + 1;
     box.put("levelStreak", levelStreak);
 
-    updateStreakProgress()
-
     if (levelStreak >= 5) {
       updateStreakStatus(
           streakIndex: levelStreakIndex,
