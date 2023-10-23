ef394f8266fdc9f415f176e29dc0c6724d472292
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-23 18:18:30 +0100

    commit

--- lib/providers/streaks.dart
+++ lib/providers/streaks.dart
@@ -6,44 +6,39 @@ List<Map<String, dynamic>> streaks = [
     "image": "assets/images/level.png",
     "streaks": [
       {
-        : {
-          "title": "Steady Progress","subtitle": "Complete 5 levels without failing",
-          "status": false,
-          "progress": 0,
-          "limit": 5,
-        }
+        "title": "Steady Progress",
+        "subtitle": "Complete 5 levels without failing",
+        "status": false,
+        "progress": 0,
+        "limit": 5,
       },
       {
-        : {
-          "title": "Leveling Up","subtitle": "Complete 10 levels without failing",
-          "status": false,
-          "progress": 0,
-          "limit": 10,
-        }
+        "title": "Leveling Up",
+        "subtitle": "Complete 10 levels without failing",
+        "status": false,
+        "progress": 0,
+        "limit": 10,
       },
       {
-        : {
-          "title": "Tenacious Triumph","subtitle": "Complete 20 levels without failing",
-          "status": false,
-          "progress": 0,
-          "limit": 20,
-        }
+        "title": "Tenacious Triumph",
+        "subtitle": "Complete 20 levels without failing",
+        "status": false,
+        "progress": 0,
+        "limit": 20,
       },
       {
-        : {
-          "title": "Masterful Streak","subtitle": "Complete 50 levels without failing",
-          "status": false,
-          "progress": 0,
-          "limit": 50,
-        }
+        "title": "Masterful Streak",
+        "subtitle": "Complete 50 levels without failing",
+        "status": false,
+        "progress": 0,
+        "limit": 50,
       },
       {
-        : {
-          "title": "Unstoppable Champion","subtitle": "Complete 100 levels without failing",
-          "status": false,
-          "progress": 0,
-          "limit": 100,
-        }
+        "title": "Unstoppable Champion",
+        "subtitle": "Complete 100 levels without failing",
+        "status": false,
+        "progress": 0,
+        "limit": 100,
       },
     ],
   },
@@ -52,39 +47,39 @@ List<Map<String, dynamic>> streaks = [
     "image": "assets/images/coins.png",
     "streaks": [
       {
-          "title": "Coin Collector","subtitle": "Earn 50 coins",
-          "status": false,
-          "progress": 0,
-          "limit": 50,
-        
+        "title": "Coin Collector",
+        "subtitle": "Earn 50 coins",
+        "status": false,
+        "progress": 0,
+        "limit": 50,
       },
       {
-          "title": "Coin Hoarder","subtitle": "Earn 100 coins",
-          "status": false,
-          "progress": 0,
-          "limit": 100,
-        
+        "title": "Coin Hoarder",
+        "subtitle": "Earn 100 coins",
+        "status": false,
+        "progress": 0,
+        "limit": 100,
       },
       {
-          "title": "Gold Gatherer","subtitle": "Earn 200 coins",
-          "status": false,
-          "progress": 0,
-          "limit": 200,
-        
+        "title": "Gold Gatherer",
+        "subtitle": "Earn 200 coins",
+        "status": false,
+        "progress": 0,
+        "limit": 200,
       },
       {
-          "title": "Treasure Hunter","subtitle": "Earn 500 coins",
-          "status": false,
-          "progress": 0,
-          "limit": 500,
-        
+        "title": "Treasure Hunter",
+        "subtitle": "Earn 500 coins",
+        "status": false,
+        "progress": 0,
+        "limit": 500,
       },
       {
-          "title": "Coin Millionaire","subtitle": "Earn 1000 coins",
-          "status": false,
-          "progress": 0,
-          "limit": 1000,
-        
+        "title": "Coin Millionaire",
+        "subtitle": "Earn 1000 coins",
+        "status": false,
+        "progress": 0,
+        "limit": 1000,
       },
     ],
   },
@@ -93,40 +88,39 @@ List<Map<String, dynamic>> streaks = [
     "image": "assets/images/answer.png",
     "streaks": {
       {
-          "title": "Trivia Beginner","subtitle": "Answer 5 questions correctly in a row",
-          "status": false,
-          "progress": 0,
-          "limit": 5,
-        
+        "title": "Trivia Beginner",
+        "subtitle": "Answer 5 questions correctly in a row",
+        "status": false,
+        "progress": 0,
+        "limit": 5,
       },
       {
-          "title": "Trivia Expert","subtitle": "Answer 10 questions correctly in a row",
-          "status": false,
-          "progress": 0,
-          "limit": 10,
-        
+        "title": "Trivia Expert",
+        "subtitle": "Answer 10 questions correctly in a row",
+        "status": false,
+        "progress": 0,
+        "limit": 10,
       },
       {
-          "title":         "Brainiac","subtitle": "Answer 20 questions correctly in a row",
-          "status": false,
-          "progress": 0,
-          "limit": 20,
-        
+        "title": "Brainiac",
+        "subtitle": "Answer 20 questions correctly in a row",
+        "status": false,
+        "progress": 0,
+        "limit": 20,
       },
       {
-          "title": "Trivia Grandmaster","subtitle": "Answer 50 questions correctly in a row",
-          "status": false,
-          "progress": 0,
-          "limit": 50,
-        
+        "title": "Trivia Grandmaster",
+        "subtitle": "Answer 50 questions correctly in a row",
+        "status": false,
+        "progress": 0,
+        "limit": 50,
       },
       {
-        
-          "title": "Trivia Maestro","subtitle": "Answer 100 questions correctly in a row",
-          "status": false,
-          "progress": 0,
-          "limit": 100,
-        
+        "title": "Trivia Maestro",
+        "subtitle": "Answer 100 questions correctly in a row",
+        "status": false,
+        "progress": 0,
+        "limit": 100,
       },
     },
   },
@@ -135,36 +129,32 @@ List<Map<String, dynamic>> streaks = [
     "image": "assets/images/rank.png",
     "streaks": {
       {
-
-          "title":         "Achiever","subtitle": "Rank top 10 on the leaderboard",
-          "status": false,
-          "progress": 0,
-          "limit": 1,
-        
+        "title": "Achiever",
+        "subtitle": "Rank top 10 on the leaderboard",
+        "status": false,
+        "progress": 0,
+        "limit": 1,
       },
       {
-        
-          "title": "Top Ranker","subtitle": "Rank top 5 on the leaderboard",
-          "status": false,
-          "progress": 0,
-          "limit": 1,
-        
+        "title": "Top Ranker",
+        "subtitle": "Rank top 5 on the leaderboard",
+        "status": false,
+        "progress": 0,
+        "limit": 1,
       },
       {
-        
-          "title": "Master Ranker","subtitle": "Rank top 3 on the leaderboard",
-          "status": false,
-          "progress": 0,
-          "limit": 1,
-        
+        "title": "Master Ranker",
+        "subtitle": "Rank top 3 on the leaderboard",
+        "status": false,
+        "progress": 0,
+        "limit": 1,
       },
       {
-        
-          "title": "Leaderboard Champion","subtitle": "Rank first on the leaderboard",
-          "status": false,
-          "progress": 0,
-          "limit": 1,
-        
+        "title": "Leaderboard Champion",
+        "subtitle": "Rank first on the leaderboard",
+        "status": false,
+        "progress": 0,
+        "limit": 1,
       },
     },
   },
@@ -173,12 +163,11 @@ List<Map<String, dynamic>> streaks = [
     "image": "assets/images/goal.png",
     "streaks": {
       {
-        
-          "title": "Ultimate Achiever","subtitle": "Finish all streaks",
-          "status": false,
-          "progress": 0,
-          "limit": 19,
-        
+        "title": "Ultimate Achiever",
+        "subtitle": "Finish all streaks",
+        "status": false,
+        "progress": 0,
+        "limit": 19,
       },
     }
   },
