4e9512609ef5ab8d28720b67ec307607c10da12a
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-24 23:09:48 +0100

    commit

--- lib/screens/streaks.dart
+++ lib/screens/streaks.dart
@@ -229,7 +229,7 @@ class _StreaksScreeenState extends State<StreaksScreeen> {
                         ],
                       ),
                   ],
-                );
+                ) : SizedBox();
                 },
               ),
               Consumer<QuestionProvider>(
