29e77380a1df51aeaed6a2a8bba3ec65e03d337c
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-24 11:21:58 +0100

    commit

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -102,6 +102,74 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
                         ),
                       ],
                     ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 200.w,
+                          height: 200.h,
+                          child: Chart(
+                            duration: const Duration(seconds: 2),
+                            layers: [
+                              ChartGroupPieLayer(
+                                items: [
+                                  [
+                                    if (goodDays != 0)
+                                      ChartGroupPieDataItem(
+                                        amount: goodDays,
+                                        color: Colors.green,
+                                        label: "Good Days",
+                                      ),
+                                    if (badDays != 0)
+                                      ChartGroupPieDataItem(
+                                        amount: badDays,
+                                        color: Colors.red,
+                                        label: "Bad Days",
+                                      ),
+                                    if (neutralDays != 0)
+                                      ChartGroupPieDataItem(
+                                        amount: neutralDays,
+                                        color: grey400,
+                                        label: "Neutral Days",
+                                      ),
+                                  ]
+                                ],
+                                settings: ChartGroupPieSettings(
+                                  radius: 30.r,
+                                  thickness: 5,
+                                  gapBetweenChartCircles: 2,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            for (var entry in entries)
+                              Row(
+                                children: [
+                                  Container(
+                                    width: 10.w,
+                                    height: 10.w,
+                                    decoration: BoxDecoration(
+                                      color: getColorForLabel(entry.key),
+                                      shape: BoxShape.circle,
+                                    ),
+                                  ),
+                                  SizedBox(width: 10.w),
+                                  Text(
+                                    "${entry.key} (${entry.value.toStringAsFixed(1)}%)",
+                                    style: TextStyle(
+                                      color: grey100,
+                                      fontSize: 16.sp,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                          ],
+                        )
+                      ],
+                    ),
                   ],
                 );
               },
