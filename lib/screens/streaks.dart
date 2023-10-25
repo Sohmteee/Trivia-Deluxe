3baeb8043ec308a275d01621427cbf28a2f9f141
Ukaegbe Somtochukwu <sohmteecodes@gmail.com>
2023-10-25 12:04:31 +0100

    commit

--- lib/screens/streaks.dart
+++ lib/screens/streaks.dart
@@ -378,7 +378,7 @@ class _StreaksScreeenState extends State<StreaksScreeen> {
                                         ],
                                       ),
                                     ),
-                                    if (item["status"])
+                                    if (!item["status"] && !item["collected"])
                                       Row(
                                         children: [
                                           const Spacer(flex: 10),
